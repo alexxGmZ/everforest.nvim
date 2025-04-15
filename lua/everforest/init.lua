e1d468881766d9eb8a67f635bd31b1ba76c3f327
alexxGmZ <algilbertgomez@gmail.com>
2025-04-15 22:01:40 +0800

    initial commit

---
+++ lua/everforest/init.lua
@@ -0,0 +1,24 @@
+local M = {}
+local generate_syntax = require("everforest.syntax").generate_syntax
+
+--- nvim-notify support
+---@param message string notify message
+---@param log_level string|nil vim.log.levels
+---@return function vim.notify
+local function notify(message, log_level)
+   return vim.notify(message, log_level, { title = "everforest.nvim" })
+end
+
+function M.setup()
+   vim.cmd([[highlight clear]])
+   vim.opt.termguicolors = true
+   vim.g.colors_name = "everforest-al"
+
+   if generate_syntax then
+      return generate_syntax()
+   else
+      return notify("colorscheme loading failed", "ERROR")
+   end
+end
+
+return M
---
+++ lua/everforest/palette.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+local palette = {
+   dark = {
+      red = "#E67E80",
+      statusline3 = "#E67E80",
+      bg_red = "#4C3743",
+      yellow = "#DBBC7F",
+      green = "#A7C080",
+      statusline1 = "#A7C080",
+      bg_green = "#3C4841",
+      blue = "#7FBBB3",
+      bg_blue = "#384B55",
+      aqua = "#83C092",
+      purple = "#D699B6",
+      orange = "#E69875",
+      fg = "#D3C6AA",
+      bg = "#272E33"
+   },
+   light = {
+      red = "#F85552",
+      statusline3 = "#E66868",
+      bg_red = "#FFE7DE",
+      yellow = "#DFA000",
+      green = "#8DA101",
+      statusline1 = "#93B259",
+      bg_green = "#f3f5d9",
+      blue = "#3A94C5",
+      bg_blue = "#ECF5ED",
+      aqua = "#35A77C",
+      purple = "#DF69BA",
+      orange = "#F57D26",
+      fg = "#5C6A72",
+      bg = "#FFFBEF"
+   },
+}
+
+function M.get_palette()
+   return palette[vim.o.background]
+end
+
+return M
